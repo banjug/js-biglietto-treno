51e055df59df4959737f9fa67f5f296579af8266
Samuele <miotto.samuele@gmail.com>
2021-10-21 15:38:57 +0200

    add 2 decimal places to initial price

--- js/script.js
+++ js/script.js
@@ -7,6 +7,7 @@ let age = parseInt(prompt("Inserisci la tua età"));
 
 // calcolare il prezzo del biglietto (km * 0.21 euro)
 let price = distance * .21;
+price = price.toFixed(2);
 
 console.log(distance);
 console.log(age);
@@ -30,10 +31,10 @@ finalPrice = finalPrice.toFixed(2);
 console.log(finalPrice);
 
 if (age < 18) {
-    document.getElementById('my_id').innerHTML = `Il prezzo del biglietto sarebbe stato ${price}&euro;, ma grazie al tuo sconto studente il prezzo finale è ${finalPrice}&euro;`;
+    document.getElementById('my_id').innerHTML = `Il prezzo del biglietto sarebbe stato ${price} &euro;, ma grazie al tuo sconto studente il prezzo finale è ${finalPrice} &euro;`;
 } else if (age > 65) {
-    document.getElementById('my_id').innerHTML = `Il prezzo del biglietto sarebbe stato ${price}&euro;, ma grazie al tuo sconto anziani il prezzo finale è ${finalPrice}&euro;`;
+    document.getElementById('my_id').innerHTML = `Il prezzo del biglietto sarebbe stato ${price} &euro;, ma grazie al tuo sconto anziani il prezzo finale è ${finalPrice} &euro;`;
 } else {
-    document.getElementById('my_id').innerHTML = `Il prezzo del biglietto è ${finalPrice}&euro;`;
+    document.getElementById('my_id').innerHTML = `Il prezzo del biglietto è ${finalPrice} &euro;`;
 }
 // mostrare all'utente il prezzo finale del biglietto
