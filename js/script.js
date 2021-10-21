11695a82a85edc56169e688707a75ba60ccbd0b6
Samuele <miotto.samuele@gmail.com>
2021-10-21 15:46:12 +0200

    remove .toFixed from price

--- js/script.js
+++ js/script.js
@@ -6,14 +6,15 @@ let distance = parseInt(prompt("Inserisci il numero di chilometri che devi perco
 let age = parseInt(prompt("Inserisci la tua età"));
 
 // calcolare il prezzo del biglietto (km * 0.21 euro)
-let price = distance * .21;
-price = price.toFixed(2);
+let price = parseFloat(distance * .21);
+// mostra sempre i primi due decimali
+// TODO
 
 console.log(distance);
 console.log(age);
 console.log(price);
 
-// calcolare lo sconto (se < 18 -20% se >65 -40%)
+// calcolare lo sconto (se < 18 -20%, se >65 -40%)
 
 let finalPrice;
 
@@ -31,10 +32,10 @@ finalPrice = finalPrice.toFixed(2);
 console.log(finalPrice);
 
 if (age < 18) {
-    document.getElementById('my_id').innerHTML = `Il prezzo del biglietto sarebbe stato ${price} &euro;, ma grazie al tuo sconto studente il prezzo finale è ${finalPrice} &euro;`;
+    document.getElementById('my_id').innerHTML = `Il prezzo del biglietto sarebbe stato di ${price} &euro;, ma grazie al tuo sconto studente il prezzo finale &egrave; di ${finalPrice} &euro;`;
 } else if (age > 65) {
-    document.getElementById('my_id').innerHTML = `Il prezzo del biglietto sarebbe stato ${price} &euro;, ma grazie al tuo sconto anziani il prezzo finale è ${finalPrice} &euro;`;
+    document.getElementById('my_id').innerHTML = `Il prezzo del biglietto sarebbe stato di ${price} &euro;, ma grazie al tuo sconto anziani il prezzo finale &egrave; di ${finalPrice} &euro;`;
 } else {
-    document.getElementById('my_id').innerHTML = `Il prezzo del biglietto è ${finalPrice} &euro;`;
+    document.getElementById('my_id').innerHTML = `Il prezzo del biglietto &egrave; di ${price} &euro;`;
 }
 // mostrare all'utente il prezzo finale del biglietto
